2fb1dd7249c5d414ec5dc03e6ffa29dbde716d74
Mauricio776 <mauuuricio@hotmail.com>
2022-01-27 11:07:45 -0300

    Se revirtieron los cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ const typesRoutes = require('./types_of_art');
 const usersRoutes = require('./users');
 const shoppingCartRoutes = require('./shopping_cart');
 const galleryRoutes = require('./gallery');
-const reviewRoutes = require('./review');
 
 const { verifySignUp,authJwt} = require('../middleware');
 
@@ -41,19 +40,15 @@ router.get(
   adminBoard
 );
 
-
-router.use("/users", [authJwt.verifyToken], usersRoutes);
+router.use("/users", [authJwt.verifyToken, authJwt.isAdmin], usersRoutes);
 
 router.use('/home', artworkRoutes);
 router.use('/types', typesRoutes);
 
 router.use('/gallery', galleryRoutes);
 router.use('/shopping', shoppingCartRoutes)
-
-router.use('/review', reviewRoutes);
-
 router.get('/', (req, res) => {
-    res.status(200).send(' > > > > ||| . . . GO TO / / / to HEROKU DEPLOYMENT . . . ||| > > >');
+    res.status(200).send(' > > > > ||| . . . GO TO /home please __ ||| > > >');
 
 });
 
