c01b9d852b7bb3c434ede4231679fab80e574b34
Mauricio776 <mauuuricio@hotmail.com>
2022-01-27 12:08:11 -0300

    initial deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -48,7 +48,7 @@ router.use('/types', typesRoutes);
 router.use('/gallery', galleryRoutes);
 router.use('/shopping', shoppingCartRoutes)
 router.get('/', (req, res) => {
-    res.status(200).send(' > > > > ||| . . . GO TO /home please __ ||| > > >');
+    res.status(200).send(' > > > > ||| . . . GO TO -/|\-/|\- HEROKU deploy . . . ||| > > >');
 
 });
 
