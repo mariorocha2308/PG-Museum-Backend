808fa6741a08b2b2a5ed6c4506a6a19d1ac6fdf1
Mauricio776 <mauuuricio@hotmail.com>
2022-01-28 18:33:49 -0300

    back functional 2 db + efectiva

--- index.js
+++ index.js
@@ -28,53 +28,59 @@ const { Role, User } = require('./src/db');
 const port = process.env.PORT || 5040;
 
 async function defaultAdmin() {
-  const userdb = await User.findOne({
-    where: {
-      id: 1
+  try {
+    const userdb = await User.findOne({
+      where: {
+        id: 1
+      }
+    })
+    if (!userdb) {
+      const user = await User.create({
+        name: "Admin",
+        username: "admin",
+        email: "admin@gmail.com",
+        password: bcrypt.hashSync("12345", 8),
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Steve_Jobs_in_1972_Pegasus_%28retouched%29.jpg/340px-Steve_Jobs_in_1972_Pegasus_%28retouched%29.jpg"
+      });
+      await user.addRoles(3);
+    } else {
+      console.log("Admin ya existe");
     }
-  })
-  if (!userdb) {
-    const user = await User.create({
-      name: "Admin",
-      username: "admin",
-      email: "admin@gmail.com",
-      password: bcrypt.hashSync("12345", 8),
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Steve_Jobs_in_1972_Pegasus_%28retouched%29.jpg/340px-Steve_Jobs_in_1972_Pegasus_%28retouched%29.jpg"
-    });
-    await user.addRoles(3);
-  } else {
-    console.log("Admin ya existe");
+  } catch (error) {
+    console.log(error);
   }
 }
 
 
 async function initial() {
-  const roldb = await Role.findAll();
-  if (roldb.length === 0) {
-    const rolUser = await Role.create({
-      id: 1,
-      name: "user"
-    });
-    const rolAdmin = await Role.create({
-      id: 3,
-      name: "admin"
-    });
-  } else {
-    console.log("Roles ya existen");
+  try {
+    const roldb = await Role.findAll();
+    if (roldb.length === 0) {
+      const rolUser = await Role.create({
+        id: 1,
+        name: "user"
+      });
+      const rolAdmin = await Role.create({
+        id: 3,
+        name: "admin"
+      });
+    } else {
+      console.log("Roles ya existen");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
 
-  // Syncing all the models at once.
-  conn.sync({ force: true }).then(() => {
-    server.listen(port, () => {
-      console.log('o|O_O|o robot Σωκράτης listening at 5040');
-      // eslint-disable-line no-console
-      defaultAdmin();
-      initial()
-    });
-    getTypes(); // <<<--- para iniciar la base de datos
-    getApiToDb();
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(port, () => {
+    console.log('o|O_O|o robot Σωκράτης listening at 5040');
+    // eslint-disable-line no-console
+    getTypes();
+    initial()
+    defaultAdmin();
   });
-
-// deployment heroku
+  getApiToDb();
+});
--- src/db.js
+++ src/db.js
@@ -65,7 +65,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-const {RefreshToken, User, Artwork, Type, Shopping_cart, Role, Review} = sequelize.models;
+const { RefreshToken, User, Artwork, Type, Shopping_cart, Role, Review } = sequelize.models;
 
 
 
