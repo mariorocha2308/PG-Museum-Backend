89000f7b2ea4306fcda3b9a196e49fc59216ee20
Mauricio776 <mauuuricio@hotmail.com>
2022-02-01 11:56:16 -0300

    to heroku

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -2,42 +2,42 @@ const { User, user_Roles } = require("../db");
 
 async function getUsersRoles(req, res, next) {
     try {
-      const userRoles = await user_Roles.findAll();
-      const users = await User.findAll();
-      if (userRoles && users) {
-        const usersRoles = users.map((user) => {
-            const userRole = userRoles.find(
-                (userRole) => userRole.userId === user.id
-            );
-            let role = "";	
-            if (userRole.roleId === 1) {
-                role = "ROLE_USER";
-            }
-            if (userRole.roleId === 2) {
-                role = "ROLE_BLOCKED";
-            }
-            if (userRole.roleId === 3) {
-                role = "ROLE_ADMIN";
-            }
-          return {
-            id: user.id,
-            name: user.name,
-            username: user.username,
-            email: user.email,
-            image: user.image,
-            roles: role
-          };
-        });
-        return res.status(200).send({ usersRoles });
-      }
+        const userRoles = await user_Roles.findAll();
+        const users = await User.findAll();
+        if (userRoles && users) {
+            const usersRoles = users.map((user) => {
+                const userRole = userRoles.find(
+                    (userRole) => userRole.userId === user.id
+                );
+                let role = "";
+                if (userRole.roleId === 1) {
+                    role = "ROLE_USER";
+                }
+                if (userRole.roleId === 2) {
+                    role = "ROLE_BLOCKED";
+                }
+                if (userRole.roleId === 3) {
+                    role = "ROLE_ADMIN";
+                }
+                return {
+                    id: user.id,
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                    image: user.image,
+                    roles: role
+                };
+            });
+            return res.status(200).send({ usersRoles });
+        }
     } catch (error) {
-      res.status(500).json({
-        message: "Error al obtener los user and roles",
-        error,
-      });
+        res.status(500).json({
+            message: "Error al obtener los user and roles",
+            error,
+        });
     }
-  }
+}
 
-  module.exports = {
+module.exports = {
     getUsersRoles,
-  };
+};
