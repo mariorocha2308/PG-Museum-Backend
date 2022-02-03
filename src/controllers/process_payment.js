fc11adfdb59e545bdb6cd393952d911edb04c9c5
mariorocha23 <virgoroch852@gmail.com>
2022-02-03 15:33:42 -0600

    add(mercadoPago) especial function

--- src/controllers/process_payment.js
+++ src/controllers/process_payment.js
@@ -22,11 +22,33 @@ async function postProcessPayment(req, res, next) {
     // )
     mercadopago.payment
         .save(payment_data)
-        .then((response) => {
+        .then(async(response) => {
+
             return res.status(response.status).json({
                 status: response.body.status,
                 status_detail: response.body.status_detail,
                 id: response.body.id,
+                date_created: response.body.date_created,
+                date_approved: response.body.date_approved,
+                operation_type: response.body.operation_type,
+                issuer_id: response.body.issuer_id-1,
+                payment_method_id: response.body.paymentMethodId,
+                payment_type_id: response.body.payment_type_id,
+                status: response.body.status,
+                status_detail: response.body.status_detail,
+                currency_id: response.body.currency_id,
+                description: response.body.description,
+                taxes_amount: response.body.taxes_amount,
+                email: response.body.payer.email,
+                identification_type: response.body.payer.identification.type,
+                identification_number: response.body.payer.identification.number,
+                card_first_six_digits: response.body.card.first_six_digits,
+                card_last_four_digits: response.body.card.last_four_digits,
+                card_expiration_month: response.body.card.expiration_month,
+                card_expiration_year: response.body.card.expiration_year,
+                card_date_created: response.body.card.date_created,
+                card_date_last_updated: response.body.card.date_last_updated,
+                cardholder: response.body.card.cardholder.name
             });
         })
         .catch((err) => {
