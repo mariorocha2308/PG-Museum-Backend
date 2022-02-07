486e4fe5ef6452bebd3364dc9669415861d01858
Mauricio776 <mauuuricio@hotmail.com>
2022-02-07 10:05:47 -0300

    mejoras tabla mercado pago y funcional post de send email

--- src/controllers/paymentDb.js
+++ src/controllers/paymentDb.js
@@ -4,17 +4,18 @@ async function postProcessPaymentDb(req, res, next) {
     try {
         // console.log("res.body es________: ", req.body);
     const processPayment = await Process_payment.create({
-        status_: req.body.status,
-        status_detail: req.body.status_detail,
+        state: req.body.status,
+        date: req.body.date_created,
+        total: req.body.total,
+        credit_card: req.body.payment_method_id,
+        userId: req.body.user_id,
+        artworksId: req.body.products,
+        
         id: req.body.id,
+        status_detail: req.body.status_detail,
         username: req.body.username,
-        products: req.body.products,
-        total_price: req.body.total,
-        date_created: req.body.date_created,
         date_approved: req.body.date_approved,
         operation_type: req.body.operation_type,
-        user_id: req.body.user_id,
-        payment_method_id: req.body.payment_method_id,
         payment_type_id: req.body.payment_type_id,
         currency_id: req.body.currency_id,
         description: req.body.description,
@@ -45,7 +46,7 @@ async function getByIdProcessPaymentDb (req, res, next) {
     try {
         const processPayment = await Process_payment.findOne({
             where: {
-                id: req.params.id
+                userId: req.params.id
             }
         });
         res.json(processPayment);
